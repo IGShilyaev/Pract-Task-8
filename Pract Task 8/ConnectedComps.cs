5fb137e3530ead51a2feb939770d1386f8b039a2
IGShilyaev <55237654+IGShilyaev@users.noreply.github.com>
2020-06-08 11:53:38 +0500

    Update ConnectedComps.cs

--- Pract Task 8/ConnectedComps.cs
+++ Pract Task 8/ConnectedComps.cs
@@ -5,15 +5,18 @@ using System.Text;
 namespace Pract_Task_8
 {
     //Класс, реализующий алгоритм поиска двусвязных компонент в графе
-    //С помощью двойного обхода в глубину
+    //С помощью обхода в глубину
     class ConnectedComps
     {
         private bool[] marked;
         private int[] id;
         private int[] low;
         private int count;
-        public List<Edge>[] Blocks { get; set; }
+        private Stack<Edge> edges;
+        private Stack<Edge> usedEdges;
+        public List<Edge>[] Blocks { get; }
         private int blockIndex;
+        private List<int> cutpoints;
 
         public ConnectedComps(Graph G)
         {
@@ -25,14 +28,20 @@ namespace Pract_Task_8
             for (int i = 0; i < G.GetVertices(); i++)
                 Blocks[i] = new List<Edge>();
             blockIndex = -1;
+            cutpoints = new List<int>();
+            edges = new Stack<Edge>();
+            usedEdges = new Stack<Edge>();
 
             for (int i = 0; i < G.GetVertices(); i++)
-                if(!marked[i]) DFS(G, i, -1);
+                if (!marked[i]) 
+                {
+                    blockIndex++; 
+                    DFS(G, i, -1);
+                    while (edges.Count > 0)
+                        if (!usedEdges.Contains(edges.Peek())) { Blocks[blockIndex].Add(edges.Peek()); usedEdges.Push(edges.Pop()); }
+                        else edges.Pop();
+                }
 
-            marked = new bool[G.GetVertices()];
-
-            for (int i = 0; i < G.GetVertices(); i++)
-                if (!marked[i]) { blockIndex++; DFS2(G, i, blockIndex, -1); }
         }
 
         private void DFS(Graph G, int curr, int prev)
@@ -40,49 +49,45 @@ namespace Pract_Task_8
             count++;
             id[curr] = count;
             low[curr] = count;
+            int children = 0;
             marked[curr] = true;
             foreach (int i in G.GetAdj(curr))
             {
                 if (i ==prev) continue;
                 if (marked[i])
                 {
+                    edges.Push(GetEdge(curr, i));
                     low[curr] = Math.Min(low[curr], id[i]);
                 }
                 else
                 {
+                    edges.Push(GetEdge(curr, i));
                     DFS(G, i, curr);
+                    children++;
                     low[curr] = Math.Min(low[curr], low[i]) ;
+                    if (prev != -1 && low[i] >= id[curr])
+                    {
+                        cutpoints.Add(curr);
+                        while (!edges.Peek().Equals(GetEdge(curr, i)))
+                            if (!Blocks[blockIndex].Contains(edges.Peek())&&!usedEdges.Contains(edges.Peek())) { Blocks[blockIndex].Add(edges.Peek());  usedEdges.Push(edges.Pop()); }
+                            else edges.Pop();
+                        usedEdges.Push(edges.Peek());
+                        Blocks[blockIndex].Add(edges.Pop());
+                        blockIndex++;
+                    }
                 }
             }
-        }
-
-        private void DFS2(Graph G, int curr, int tekBlock, int prev)
-        {
-            marked[curr] = true;
-            foreach (int x in G.GetAdj(curr))
+            if (prev == -1 && children >= 2)
             {
-                if (x.Equals(prev)) continue;
-                if (!marked[x])
-                {
-                    if (low[x]>=id[curr])
-                    {
-                        DFS2(G, x, tekBlock, curr);
-                        tekBlock++;
-                        Blocks[tekBlock].Add(GetEdge(curr, x));
-                    }
-                    else
-                    {
-                        DFS2(G, x, tekBlock, curr);
-                        Blocks[tekBlock].Add(GetEdge(curr, x));
-                    }
-                }
-                else
-                {
-                    if (id[x] < id[curr]) Blocks[tekBlock].Add(GetEdge(curr, x));
-                }
+                cutpoints.Add(curr);
+                while (!edges.Peek().Equals(GetEdge(curr, prev)))
+                    if (!Blocks[blockIndex].Contains(edges.Peek()) && !usedEdges.Contains(edges.Peek())) { Blocks[blockIndex].Add(edges.Peek()); usedEdges.Push(edges.Pop()); }
+                    else edges.Pop();
+                usedEdges.Push(edges.Peek());
+                Blocks[blockIndex].Add(edges.Pop());
+                blockIndex++;
             }
         }
-
 
         private Edge GetEdge(int a, int b)
         {
